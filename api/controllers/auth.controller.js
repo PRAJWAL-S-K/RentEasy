9940ea90deccc94869c4e0815735d4bd4234268a
Sanmit243 <243sanmitpatole@gmail.com>
2024-08-18 16:46:14 +0530

    created sign up API route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signUp =async (req, res)=>{
+    const {username, email, pass}=req.body;
+
+    const hashedPass = bcryptjs.hashSync(pass,10); //to encrypt the pass with some salt variable 10
+    try{ //to handle duplicate key error
+        const newUser = new User({username,email,password:hashedPass});
+        await newUser.save(); //it will take some time so made await
+        res.status(201).json("The info got saved succesefully!"); //status 201 means something has got saved.
+    }
+    catch(error)
+    {
+        res.status(500).json(error.message); //status 500 means some error has occured
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 mongoose
   .connect(process.env.MANGO)
@@ -11,10 +13,12 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json()); //it will allow json to show in terminal
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
  
