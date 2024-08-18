f2a478affff600d55ddccf0a39be0e3b110f1167
Sanmit243 <243sanmitpatole@gmail.com>
2024-08-18 15:59:06 +0530

    Created a test API route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose
   .connect(process.env.MANGO)
   .then(() => {
     console.log("DataBase connected successfully!");
   })
-  .catch((err) => console.log(err)); 
+  .catch((err) => console.log(err));
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000!");
 });
+
+app.use('/api/user',userRouter);
+ 
