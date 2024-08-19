60f1a01133c8c76374f18768edfd7a4c669fc4ee
PRAJWAL-S-K <skprajwal333@gmail.com>
2024-08-19 19:32:25 +0530

    update header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,28 @@
 import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
+import PrivateRoute from "./components/PrivateRoute";
+
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
+import Profile from "./pages/Profile";
 import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
+<Route element={<PrivateRoute />}>
+  <Route path="/profile" element={<Profile />} />
+</Route>;
 import Header from './components/Header';
 export default function App() {
-  return <BrowserRouter>
-  <Header/>
-  <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/sign-in" element={<SignIn/>}></Route>
-      <Route path="/sign-up" element={<SignUp/>}></Route>
-      <Route path="/profile" element={<Profile/>}></Route>
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,42 +1,51 @@
 import React from 'react'
-import {FaSearch} from 'react-icons/fa'; //to add icons
+import { FaSearch } from 'react-icons/fa'; //to add icons
 import { Link } from 'react-router-dom'; //to link items to different pages
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-purple-100 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      
-      {/* icon */}
-      <Link to="/">
-      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-purple-600'>RENT</span>
-          <span className='text-purple-900'>easy</span>
-      </h1>
-      </Link>
 
-      {/* search bar */}
-      <form className='bg-purple-50 p-3 rounded-lg flex items-center'>
-        <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-        <FaSearch className='text-purple-600'/>
-      </form>
-
-      {/* menu */}
-      <ul className='flex gap-4 '>
+        {/* icon */}
         <Link to="/">
-        <li className='hidden sm:inline text-purple-700 hover:underline'>
-          Home
-        </li>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-purple-600'>RENT</span>
+            <span className='text-purple-900'>easy</span>
+          </h1>
         </Link>
-        <Link to="/about">
-        <li className='hidden sm:inline text-purple-700 hover:underline'>
-        About
-        </li>
-        </Link>
-        <Link to="/sign-in">
-        <li className='text-purple-700 hover:underline'>Sign in</li>
-        </Link>
-      </ul>
+
+        {/* search bar */}
+        <form className='bg-purple-50 p-3 rounded-lg flex items-center'>
+          <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+          <FaSearch className='text-purple-600' />
+        </form>
+
+        {/* menu */}
+        <ul className='flex gap-4 '>
+          <Link to="/">
+            <li className='hidden sm:inline text-purple-700 hover:underline'>
+              Home
+            </li>
+          </Link>
+
+          <Link to="/about">
+            <li className='hidden sm:inline text-purple-700 hover:underline'>
+              About
+            </li>
+          </Link>
+
+          <Link to='/profile'>
+            {currentUser ? (
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
+
+        </ul>
       </div>
     </header>
   )
