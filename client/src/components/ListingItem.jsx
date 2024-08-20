effbd197eeb5d19fd394b6cf1a4b1ad311734ce7
PRAJWAL-S-K <skprajwal333@gmail.com>
2024-08-21 02:18:06 +0530

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -26,14 +26,14 @@ export default function ListingItem({ listing }) {
           <p className='text-sm text-gray-600 line-clamp-2'>
             {listing.description}
           </p>
-          <p className='text-purple-500 mt-2 font-semibold '>
+          <p className='text-slate-500 mt-2 font-semibold '>
           ₹
             {listing.offer
               ? listing.discountedPrice.toLocaleString('en-IN')
               : listing.regularPrice.toLocaleString('en-IN')}
             {listing.type === 'rent' && ' / month'}
           </p>
-          <div className='text-purple-700 flex gap-4'>
+          <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
               {listing.bedrooms > 1
                 ? `${listing.bedrooms} beds `
