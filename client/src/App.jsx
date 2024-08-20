5656110f46e2891ed6ec59a75f7c70025dbc2d60
PRAJWAL-S-K <skprajwal333@gmail.com>
2024-08-20 19:52:56 +0530

    add image slider to the listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import {BrowserRouter, Routes, Route, Router} from 'react-router-dom';
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
-
 import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import Profile from "./pages/Profile";
 import SignUp from './pages/SignUp';
+import Listing from "./pages/Listing";
+
+
 <Route element={<PrivateRoute />}>
   <Route path="/profile" element={<Profile />} />
 </Route>;
@@ -21,11 +23,13 @@ export default function App() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/listing/:listingId" element={<Listing />} />
+
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path="/create-listing" element={<CreateListing />} />
           <Route
-            path='/update-listing/:listingId'
+            path="/update-listing/:listingId"
             element={<UpdateListing />}
           />
         </Route>
