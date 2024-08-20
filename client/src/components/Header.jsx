4735f278638d4c4b8a38981167f688e8470d29a8
Sanmit243 <243sanmitpatole@gmail.com>
2024-08-20 23:23:54 +0530

    completed header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,52 +1,85 @@
-import React from 'react'
-import { FaSearch } from 'react-icons/fa'; //to add icons
-import { Link } from 'react-router-dom'; //to link items to different pages
+import React from "react";
+import { FaSearch } from "react-icons/fa"; //to add icons
+import { Link, useNavigate } from "react-router-dom"; //to link items to different pages
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  return (
-    <header className='bg-purple-100 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <header className="bg-purple-100 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         {/* icon */}
         <Link to="/">
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-purple-600'>RENT</span>
-            <span className='text-purple-900'>easy</span>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-purple-600">RENT</span>
+            <span className="text-purple-900">easy</span>
           </h1>
         </Link>
 
         {/* search bar */}
-        <form className='bg-purple-50 p-3 rounded-lg flex items-center'>
-          <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-          <FaSearch className='text-purple-600' />
+        <form
+          onSubmit={handleSubmit}
+          className="bg-purple-50 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-purple-600" />
+          </button>
         </form>
 
         {/* menu */}
-        <ul className='flex gap-4 '>
+        <ul className="flex gap-4 ">
           <Link to="/">
-            <li className='hidden sm:inline text-purple-700 hover:underline'>
+            <li className="hidden sm:inline text-purple-700 hover:underline">
               Home
             </li>
           </Link>
 
           <Link to="/about">
-            <li className='hidden sm:inline text-purple-700 hover:underline'>
+            <li className="hidden sm:inline text-purple-700 hover:underline">
               About
             </li>
           </Link>
 
-          <Link to='/profile'>
+          <Link to="/profile">
             {currentUser ? (
-              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
             ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              <li className=" text-purple-700 hover:underline"> Sign in</li>
             )}
           </Link>
-
         </ul>
       </div>
     </header>
-  )
+  );
 }
